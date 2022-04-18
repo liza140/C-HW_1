88c23fb6ada8f287a1438357f9b913dcd2d01b4c
liza140 <liza140@mail.ru>
2022-04-18 23:04:07 +0300

    Максимальное из трех чисел

---
+++ EX_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Программа принимает на вход три числа и выдаёт максимальное из этих чисел
+
+int a = 7;
+int b = 5;
+int c = 7;
+int max = a;
+if (a<b)
+{
+    if (c<b)
+    {
+        max = b;
+    }
+    else 
+    {
+        max = c;
+    }
+}
+else 
+{
+    if (a<c)
+    {
+        max = c; 
+    }
+    else 
+    {
+        max=a;
+    }
+}
+Console.Write("Max = ");
+Console.WriteLine(max);
