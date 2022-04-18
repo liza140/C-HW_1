050c0cade19b91739d4bdac73b458ed1457a6e43
liza140 <liza140@mail.ru>
2022-04-18 23:37:49 +0300

    Четные числа от 1 до N

---
+++ EX_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = 0;
+if (N > 1)
+{
+    int count = 1;
+    while (count < N + 1)
+    {
+        if (count % 2 == 0) Console.WriteLine(count);
+        count++;
+    }
+}
+else Console.WriteLine("Операция невозможна");
