7b988c6a9b51f5608874feac4fec048d2fb2ffa5
liza140 <liza140@mail.ru>
2022-04-18 23:21:07 +0300

    Оптимизация кода

--- EX_2/Program.cs
+++ EX_2/Program.cs
@@ -1,30 +1,18 @@
 ﻿// Задача 2: Программа принимает на вход три числа и выдаёт максимальное из этих чисел
 
-int a = 7;
+int a = 44;
 int b = 5;
-int c = 7;
+int c = 78;
 int max = a;
 if (a < b)
 {
-    if (c < b)
-    {
-        max = b;
-    }
-    else
-    {
-        max = c;
-    }
+    if (c < b) max = b;
+    else max = c;
 }
 else
 {
-    if (a < c)
-    {
-        max = c;
-    }
-    else
-    {
-        max = a;
-    }
+    if (a < c) max = c;
+    else max = a;
 }
 Console.Write("Max = ");
 Console.WriteLine(max);
