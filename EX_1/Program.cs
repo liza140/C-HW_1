e6e26a5009d27e2a1d7b4d2dac4296c742d5d9d5
liza140 <liza140@mail.ru>
2022-04-18 19:49:26 +0300

    Поиск максимального числа из 2х

---
+++ EX_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = -9;
+int b = -3;
+if (a>b)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.Write("Max = ");
+    Console.WriteLine(b);
+}
