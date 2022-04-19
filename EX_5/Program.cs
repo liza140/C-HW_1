544d0aa650fd699e40a8141e11c224a985ef1840
liza140 <liza140@mail.ru>
2022-04-19 20:27:00 +0300

    Числа от -N до N

---
+++ EX_5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+int N = 4;
+int count = -N;
+if (N > 0)
+{
+    while (count < N + 1)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+}
+else
+{
+    while (count > N - 1)
+    {
+        Console.WriteLine(count);
+        count = count - 1;
+    }
+}
